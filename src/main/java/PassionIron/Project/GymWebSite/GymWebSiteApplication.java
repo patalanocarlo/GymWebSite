77e5e79a427deb79f4317bf4745c04605623ce35
patalanocarlo <157119840+patalanocarlo@users.noreply.github.com>
2024-09-09 09:51:47 +0200

    Initial commit

---
+++ src/main/java/PassionIron/Project/GymWebSite/GymWebSiteApplication.java
@@ -0,0 +1,13 @@
+package PassionIron.Project.GymWebSite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymWebSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymWebSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/PassionIron/Project/GymWebSite/GymWebSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package PassionIron.Project.GymWebSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymWebSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
