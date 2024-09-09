1eefa17411fee22f3ec017bcf67091517b65f0ce
patalanocarlo <157119840+patalanocarlo@users.noreply.github.com>
2024-09-09 10:41:09 +0200

    fine prime configurazioni

--- src/main/java/PassionIron/Project/GymWebSite/Entities/Utente.java
+++ src/main/java/PassionIron/Project/GymWebSite/Entities/Utente.java
@@ -1,12 +1,13 @@
-package Entities;
+package PassionIron.Project.GymWebSite.Entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Utente {
-    @GeneratedValue
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 }
